d8bc0f7e49fac4dd61771f9ba7904ee69116a848
TeeJay64225 <kwesilit@gmail.com>
2025-06-28 15:53:31 +0000

    fixed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/customer-segmentation-app-frontend",
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
